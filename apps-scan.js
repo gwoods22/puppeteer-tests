929f572a2f54595497cf626a4b6c98622ff5abea
Graeme Woods <graemewoods202@gmail.com>
2020-12-16 23:20:13 -0500

    add app updated checker with reference file apps.json

---
+++ apps-scan.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer');
+const apps = require('./apps.json').apps
+const open = require('mac-open');
+
+let launchOptions = {
+    headless: true
+}
+async function checkPiratedApps() {
+    const browser = await puppeteer.launch(launchOptions);
+    const page = await browser.newPage();    
+    
+    let results = await (async function loop(page, apps) {
+        let promises = [];
+        for (let i = 0; i < apps.length; i++) {
+            promises.push(await load(page, apps[i]));
+            console.log(`Scraping... ${parseInt((i+1)/apps.length*100)}%`);
+        }
+        // when all promises have been resolved continue
+        return Promise.allSettled(promises);
+    })(page, apps)
+
+    browser.close()
+
+    results = results
+                .map(x => x.value)
+                .filter(x => !x.new );
+    
+    if ( results.length) {
+        console.log("----Out of date apps----");
+        for (let i = 0; i < results.length; i++) {
+            console.log("  ",results[i].name,"\n  ",results[i].url);
+        }
+        open("~/Documents/Projects/puppeteer-tests/apps.json")
+    } else {
+        console.log("All apps are up to date!");
+    }
+
+}
+
+async function load(page, app) {
+    await page.goto(app.url);
+    let newName = await page.$eval(
+        '#search-entries > article:first-child .search-entry-header-title a',
+        x => x.innerText
+    );
+    app.new = newName === app.name
+    return app
+}
+
+checkPiratedApps()
