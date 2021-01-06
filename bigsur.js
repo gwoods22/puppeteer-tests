04d71b0f2521f1ba0963194058f7b2e949a62a62
Graeme Woods <graemewoods202@gmail.com>
2021-01-05 23:18:21 -0500

    update version check

--- bigsur.js
+++ bigsur.js
@@ -6,12 +6,13 @@ let launchOptions = {
 async function checkBigSur() {
     const browser = await puppeteer.launch(launchOptions);
     const page = await browser.newPage();
+    const version = "Version 11.1"
 
     await page.goto('https://apps.apple.com/ca/app/macos-big-sur/id1526878132'); 
 
-    let version = await page.$eval('.whats-new__latest__version', el => el.innerText)
+    let newVersion = await page.$eval('.whats-new__latest__version', el => el.innerText)
 
-    let message = version !== "Version 11.0.1" ? `UPDATE! Big Sur is now on ${version}` : 'No update yet'
+    let message = version !== newVersion ? `UPDATE! Big Sur is now on ${version}` : `Still on ${version}`
 
     console.log(message);
 
